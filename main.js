c046173d94ec962223183b4b188601855f3ea1ff
Sinem Sungur <ssinemsungur@gmail.com>
2020-11-22 20:32:55 +0300

    style.css added

---
+++ main.js
@@ -0,0 +1,46 @@
+// NAVBAR
+const toggleBtn = document.getElementsByClassName("toggle-btn")[0];
+const navbarLinks = document.getElementsByClassName("navbar-links")[0];
+
+toggleBtn.addEventListener("click", () => {
+  navbarLinks.classList.toggle("active");
+});
+
+// FORM
+
+const form = document.getElementById("form");
+const username = document.getElementById("name");
+const email = document.getElementById("email");
+const phone = document.getElementById("phone");
+const pickedFDate = document.getElementById("fdate");
+var currentDate = new Date()
+
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
+  checkInputs();
+});
+
+function checkInputs() {
+  if(name.value == ""){
+    alert("Lütfen Hiçbir Alanı Boş Bırakmayınız");
+    phone.focus();
+    return false;
+  }
+
+  if (email.value == "") {
+    alert("Lütfen Hiçbir Alanı Boş Bırakmayınız");
+    email.focus();
+    return false;
+  }
+  if (phone.value === "") {
+    alert("Lütfen Hiçbir Alanı Boş Bırakmayınız");
+    phone.focus();
+    return false;      
+  }
+  if (pickedFDate.value < currentDate.value) {
+    alert("İleri tarihli bir gün seçiniz");
+    return false;
+  }
+
+  return true;
+};
